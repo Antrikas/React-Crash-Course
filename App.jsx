23d27c243bb90eef12b9d1aa254a32abe83188ff
Antrikas <andreas.antoniades17@gmail.com>
2023-12-11 13:00:44 +0200

    rcc

---
+++ App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+   <h1>My Todo List</h1>
+   <div>
+    <h2>Finish Frontend Simplified</h2>
+    <button>Delete</button>
+   </div>
+   <div>
+   <h2>Finish the Interview Section</h2>
+    <button>Delete</button>
+   </div>
+   <div>
+   <h2>Land a $100k Job</h2>
+   <button>Delete</button>
+  </div>
+  </div>
+  );
+}
+
+export default App;
