ae343c767b4dd5cdbf168c913e806c91a0d82788
Antrikas <andreas.antoniades17@gmail.com>
2023-12-13 13:43:04 +0200

    rcc

--- App.jsx
+++ App.jsx
@@ -2,9 +2,39 @@ import './App.css';
 import Todo from './components/Todo.jsx'
 import Title from './components/Title.jsx'
 import Modal from './components/Modal.jsx'
+import Counter from './components/Counter.jsx'
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
+  const [showModal, setShowModal] = useState(false)
+ 
+  function onTodoDelete (){
+    setShowModal (true)
+  }
+
+  function cancelModal(){
+    setShowModal (false);
+  }
+
+  function confirmModal(){
+    setShowModal (false);
+  }
+
+useEffect(() => {
+  console.log('ONLY on mount')
+  setShowModal(!showModal)
+}, [])
+
+useEffect(() => {
+console.log(`on mount AND on &{showModal} change`)
+}, [showModal])
+
+useEffect (() => {
+  console.log(`EVERY render`)
+})
+
+
   return (
     <div >
    <Title />
@@ -12,20 +42,23 @@ function App() {
     <input type="text" onChange={(event) => {
       console.log(event.target.value)
     }} />
-    <button>Add todo</button>
+    <button onClick={() => setShowModal(true)}>Add todo</button>
    </div>
    <div className='todo__wrapper'>
-   <Todo 
-   title="Finish Frontend Simplified" 
-  paragraph="Code along with Frontend Simplified step by step"/>
-   <Todo 
-   title=" Finish Interview Section"
-   paragraph=" Finish every interview question in the next 6 weeks "/>
-   <Todo 
-   title="Land a 100k Job"
-   paragraph=" apply to 100 jobs" />
+   <Todo onTodoDelete ={onTodoDelete}
+   title="Finish Frontend Simplified" />
+   <Todo onTodoDelete ={onTodoDelete}
+   title=" Finish Interview Section"/>
+   <Todo onTodoDelete ={onTodoDelete}
+   title="Land a 100k Job"/>
   </div>
-   <Modal title="Are you sure you want to delete?"/> 
+   {showModal && (
+   <Modal 
+   cancelModal={cancelModal} 
+   confirmModal={confirmModal}  
+    title="Confirm delete?"
+    /> 
+  )}
   </div>
 );
 }
