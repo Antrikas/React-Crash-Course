8af806a9524ef615b4de650be1a4bbf73a5d378a
Antrikas <andreas.antoniades17@gmail.com>
2023-12-14 12:29:48 +0200

    rcc

--- App.jsx
+++ App.jsx
@@ -9,7 +9,6 @@ function App() {
  
   return (
    <Router>
-    <Nav />
     <Routes>
     <Route path="/" element={<Home/>} />
     <Route path="/users/:id" element={<Users />} />
--- pages/Users.jsx
+++ pages/Users.jsx
@@ -1,10 +1,29 @@
-import {useParams} from 'react-router-dom'
+import {Link, useParams} from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 function Users () {
     const { id } = useParams ()
+ const [user, setUser] = useState({})
+  
+async function fetchUsers () {
+  const {data } = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+setUser(data)
 
-   // "https://jsonplaceholder.typicode.com/users"
-    return <h1>{id}</h1>
+}
+
+useEffect (() =>{
+    fetchUsers()
+}, [])
+    return (
+    <div>
+        <Link to="/">Go Back</Link>
+        <p>{user.id}</p>
+        <p>{user.name}</p>
+        <p>{user.email}</p>
+        <p>{user.username}</p>
+    </div>
+    )
 }
 
 export default Users
